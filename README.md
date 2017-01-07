f46a6b93d45dca2fe4066eb1e8286d7c375015ed
Michael Glitzos <Michael Glitzos>
2017-01-07 11:34:52 -0500

    Documentation gets generated

---
+++ README.md
@@ -0,0 +1,1 @@
+# React ESnext Webpack Skeleton
--- __tests__/test-app.js
+++ __tests__/test-app.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 
 import App from '../app/components/app';
 
-it("tests App component", function(){
-    const app = shallow(<App />);
-    expect(app.text()).toContain("Hello from React!");
+describe("App component tests", function () {
+    it("Test App component moutning", function () {
+        const app = shallow(<App />);
+        expect(app.text()).toContain("Hello from React!");
+    });
 });
